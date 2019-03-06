3955663f5f6bb9d6e9ca0d459f513f104a6ab2a7
Sean Kimball <smkimbal@gmail.com>
2019-03-06 01:09:25 -0700

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-jumbotron src="https://images.unsplash.com/photo-1463412855783-af97e375664b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" style="position: fixed; width: 100%; height: 50%; left: 0"></v-jumbotron>
+    <v-layout
+      justify-center
+      text-xs-center
+      wrap
+    >
+      <v-flex xs12 md6 class="mt-5">
+        <profile-card />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import ProfileCard from '@/components/ProfileCard'
+
+  export default {
+    components: {
+      ProfileCard,
+    },
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader'
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify'
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify'
+        }
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com'
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com'
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuetifyjs.com'
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs'
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify'
+        }
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer'
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/layout/pre-defined'
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+        }
+
+      ]
+    })
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-layout
+        row
+        wrap
+      >
+        <v-flex xs12 md5 class="pr-3">
+          <v-img
+            style="border-radius: 50%;"
+            src="https://avatars0.githubusercontent.com/u/25789424?s=400&u=0980d05a1e23eb957eda46af38f774829cb9df0a&v=4"
+          />
+        </v-flex>
+        <v-flex xs12 md7 text-xs-left>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-card-text class="display-1 pb-0">
+                <span class="font-weight-light">I'm</span><span class="font-weight-bold"> Sean Kimball</span>
+              </v-card-text>
+            </v-flex>
+            <v-flex xs12>
+              <v-card-text class="subheading pt-0">
+                Full-stack Developer and Manager
+              </v-card-text>
+              <v-divider style="margin-left: 15px"/>
+            </v-flex>
+            <v-flex xs12>
+              <v-data-iterator
+                :items="[
+                  { email: 'smkimbal@gmail.com', phone: '+1 480 462 8557', availability: 'Part-time' }
+                ]"
+                content-tag="v-layout"
+                hide-actions
+                row
+                wrap
+              >
+                <template v-slot:item="props">
+                  <v-flex xs12 md8>
+                    <v-card flat>
+                      <v-list dense>
+                        <v-list-tile>
+                          <v-list-tile-content>Email:</v-list-tile-content>
+                          <v-list-tile-content class="align-end">{{ props.item.email }}</v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-content>Phone:</v-list-tile-content>
+                          <v-list-tile-content class="align-end">{{ props.item.phone }}</v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-content>Availability:</v-list-tile-content>
+                          <v-list-tile-content class="align-end">{{ props.item.availability }}</v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile>
+                          <v-list-tile-content>Coffee Per Day:</v-list-tile-content>
+                          <v-list-tile-content class="align-end">
+                            <v-chip label color="primary">
+                              3 Gal<v-icon class="pl-2">mdi-coffee</v-icon>
+                            </v-chip>
+                          </v-list-tile-content>
+                        </v-list-tile>
+                      </v-list>
+                    </v-card>
+                  </v-flex>
+                </template>
+              </v-data-iterator>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-divider />
+    <v-card-actions class="primary">
+        <v-layout>
+          <v-flex>
+            <v-btn icon class="mx-4">
+              <v-icon>mdi-facebook</v-icon>
+            </v-btn>
+            <v-btn icon class="mx-4">
+              <v-icon>mdi-github-circle</v-icon>
+            </v-btn>
+            <v-btn icon class="mx-4">
+              <v-icon>mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn icon class="mx-4">
+              <v-icon>mdi-linkedin</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
